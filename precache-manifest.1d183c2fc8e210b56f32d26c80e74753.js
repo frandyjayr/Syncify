4909007590f96340865b2b50e0ac3da0a5f6a4a3
Frandy Jay-R Usi <frandyjayr@gmail.com>
2020-03-06 22:57:29 -0800

    Updates

---
+++ precache-manifest.1d183c2fc8e210b56f32d26c80e74753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f207fd297123dcff63d470b6e6b75333",
+    "url": "/Syncify/index.html"
+  },
+  {
+    "revision": "4997f5ab130295970470",
+    "url": "/Syncify/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "be62dffd75d55582220f",
+    "url": "/Syncify/static/css/main.ee8352ce.chunk.css"
+  },
+  {
+    "revision": "4997f5ab130295970470",
+    "url": "/Syncify/static/js/2.c7b9e393.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Syncify/static/js/2.c7b9e393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be62dffd75d55582220f",
+    "url": "/Syncify/static/js/main.ff51dc75.chunk.js"
+  },
+  {
+    "revision": "2fb9fb908c7d524f3f17",
+    "url": "/Syncify/static/js/runtime-main.0e6a0dce.js"
+  }
+]);
